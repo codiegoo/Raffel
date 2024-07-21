4f0a5bc9def3e9a64126eed7e33f60483ec16f39
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2024-07-21 09:02:50 -0700

    add alt

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
-  
+  images: {
+    domains: ['https://sorteos-jp.vercel.app'], // Dominio para el que se optimizarán las imágenes
+    loader: 'default',
+    path: '/_next/image',
+    remotePatterns: [
+      {
+        protocol: 'https',           // Protocolo de la URL remota
+        hostname: 'sorteos-jp.vercel.app',  // Nombre del host (dominio) de la URL remota
+        port: '',                    // Puerto (opcional, deja vacío si no es relevante)
+        pathname: '/icon.png',       // Ruta específica o patrón de ruta que deseas optimizar
+      }
+    ]
+  }
 };
 
 export default nextConfig;
