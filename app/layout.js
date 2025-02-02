5828dc966c19063b3285d93a48bc806729932411
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2025-02-01 21:47:10 -0600

    verify

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,27 @@
 import { Inter } from "next/font/google"
 import 'bootstrap/dist/css/bootstrap.css'
 import Boostrap from "@/components/Boostrap/Boostrap"
-import Head from 'next/head'
 import './main.sass'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sorteos JP ",
+  title: "Sorteos JP",
   description: "Pagina web de sorteos entre amigos los mochis culiacan",
-  ogImage: '/images/logo.png',
-  facebookDomainVerification: "zdm34a265j0uckz3hqiwinpxpj1oa9"
+  openGraph: {
+    title: "Sorteos JP",
+    description: "Pagina web de sorteos entre amigos los mochis culiacan",
+    images: ['/images/logo.png'], // Usa URL absoluta si hay problemas
+    type: "website",
+  },
+  other: {
+    "facebook-domain-verification": "zdm34a265j0uckz3hqiwinpxpj1oa9"
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Etiqueta meta para Open Graph con ruta relativa */}
-        <meta property="og:image" content={metadata.ogImage} />
-        {/* Otras etiquetas meta que necesites */}
-        <meta name="facebook-domain-verification" content={metadata.facebookDomainVerification} />
-      </Head>
       <body className={inter.className}>
         {children}
         <Boostrap/>
--- app/page.js
+++ app/page.js
@@ -4,10 +4,14 @@ import Inicio from "@/layouts/Inicio/Inicio";
 import Pagos from "@/layouts/Pagos/Pagos";
 import './main.sass'
 import Footer from "@/layouts/Footer/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="facebook-domain-verification" content="zdm34a265j0uckz3hqiwinpxpj1oa9" />
+      </Head>
       <Inicio/>
       <Boletos/>
       <Pagos/>
